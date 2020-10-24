b1e8c12805dfd5127195c209dba4affbcfd88f37
Veneti <briantruong341675@gmail.com>
2020-10-23 19:28:30 -0500

    import pic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import edmond from "./pictures/edmond.jpg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={edmond} className="App-logo" alt="logo" />
+				<p>Keyforge Edmond?</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Lets play keyforge rn
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
